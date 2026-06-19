1cda3abc0764f1f075af0350c7370c89b7ad4cca
holabendez <nick@holabendez.com>
2026-06-19 13:15:25 -0400

    fix: remove unused ts-expect-error directive

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // @ts-expect-error Vite and Vitest peer dependency conflict
   plugins: [react()],
 })
